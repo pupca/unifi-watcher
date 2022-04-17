f5c8a869cc868256089c7b2d34235b1ffb38e1e6
Popelak, Martin <pupca@pupca.cz>
2022-04-17 19:14:39 +0200

    support variants

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ import json
 import time
 import pickledb
 import re
-from lxml import etree
-from io import StringIO
-from lxml.cssselect import CSSSelector
 from dotenv import load_dotenv
 import telepot
 from datetime import datetime
 import traceback
 import sys
-
 
 
 import logging
@@ -29,23 +25,25 @@ logging.basicConfig(
 load_dotenv()
 
 bot = telepot.Bot(os.environ.get("BOT_TOKEN"))
-
 botToken = os.environ.get("BOT_TOKEN")
+timeOut = os.getenv("TIMEOUT", "60")
+variantId = os.getenv("VARIANT", False)
 s = requests.Session()
-parser = etree.HTMLParser()
-selector = CSSSelector('.bundleLogic__wizard .comProduct__badge')
 db = pickledb.load('unifyGuard.db', True)
 url = os.environ.get("ITEM_URL")
-itemName = os.environ.get("ITEM_NAME")
+itemName = os.getenv("ITEM_NAME", "")
 
-logging.info(f"App starting to watch item: '{itemName}' on url: {url}")
+
+logging.info(
+    f"App starting to watch item. Name:{itemName} Variant:'{variantId}', timeout:'{timeOut}', url:'{url}'")
 
 while True:
     try:
         logging.info(f"Getting the item url.")
         response = s.get(url)
         html = response.content.decode("utf-8")
-        logging.info(f"Got response [{response.status_code}]. Parsing the page.")
+        logging.info(
+            f"Got response [{response.status_code}]. Parsing the page.")
         with open("log.html", "w+") as file:
             file.write(html)
 
@@ -53,22 +51,49 @@ while True:
             logging.info(f"Sending health check.")
             db.set('health', datetime.today().strftime('%Y-%m-%d'))
             bot.sendMessage(os.environ.get("BOT_CHAT"),
-                            f"Still watching item: {itemName}...")
+                            f"Still watching item {itemName}...")
 
         match = re.findall(r"var json_product = (.*\"});", html)
         logging.info(f"JSON parsed.")
+
         if match:
+            available = False
             json_match = json.loads(match[0])
-            if json_match.get('available', ''):
+            with open("log.json", "w+") as file:
+                file.write(match[0])
+
+            if variantId:
+                variants = json_match.get("variants", [])
+                variant = False
+                for var in variants:
+                    if variantId == var.get('id', ''):
+                        variant = var
+
+                if variant:
+                    if variant.get('available', False):
+                        available = True
+                else:
+                    if db.get('variant_error') != datetime.today().strftime('%Y-%m-%d'):
+                        db.set('variant_error',
+                               datetime.today().strftime('%Y-%m-%d'))
+                        bot.sendMessage(os.environ.get("BOT_CHAT"),
+                                        f"Error watching item {itemName}. Variant {variantId} was not found on the request page.")
+
+            else:
+                if json_match.get('available', False):
+                    available = True
+
+            if available:
                 logging.info("Item is in stock")
 
                 if db.get(url) == datetime.today().strftime('%Y-%m-%d'):
-                    logging.info("Notification was sent already today. Skiping")
+                    logging.info(
+                        "Notification was sent already today. Skiping")
                 else:
                     logging.info("Sending notification")
                     db.set(url, datetime.today().strftime('%Y-%m-%d'))
                     bot.sendMessage(os.environ.get("BOT_CHAT"),
-                                    f"Your Item: {itemName} is in stock! {url}")
+                                    f"Your Item {itemName} is in stock! {url}")
             else:
                 logging.info("Item is sold out")
     except:
@@ -77,6 +102,6 @@ while True:
         if db.get('error') != datetime.today().strftime('%Y-%m-%d'):
             db.set('error', datetime.today().strftime('%Y-%m-%d'))
             bot.sendMessage(os.environ.get("BOT_CHAT"),
-                            f"Error watching item: {itemName}. Error:'{sys.exc_info()[0]}' check the logs.")
+                            f"Error watching item {itemName}. Error:'{sys.exc_info()[0]}' check the logs.")
 
-    time.sleep(60)
+    time.sleep(int(timeOut))
--- readme.md
+++ readme.md
@@ -4,9 +4,11 @@ This is a small python library used to monitor the unifi store and alert via tel
 
 # Env Variables
 
-| Parameter | Function |
-| :----: | --- |
-| `-e BOT_TOKEN=XXX` | Telegram Bot Token |
-| `-e BOT_CHAT=XXX` | Telegram Group/Chat ID |
-| `-e ITEM_URL=XXX` | URL of Unifi Item |
-| `-e ITEM_NAME=XXX` | Name of the Unifi Item |
+|     Parameter      | Function                                                                     |
+| :----------------: | ---------------------------------------------------------------------------- |
+| `-e BOT_TOKEN=XXX` | Telegram Bot Token                                                           |
+| `-e BOT_CHAT=XXX`  | Telegram Group/Chat ID                                                       |
+| `-e ITEM_URL=XXX`  | URL of Unifi Item                                                            |
+|  `-e VARIANT=XXX`  | Variant of Unifi Item (optional, if only one variant is present on the page) |
+| `-e ITEM_NAME=XXX` | Name of the Unifi Item                                                       |
+|  `-e TIMEOUT=XXX`  | Number of seconds to wait between retries                                    |
